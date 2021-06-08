df05f069b10d6cd91e54aeedb67fba23eecfff0e
ludovicoabrate <ludovicoabrate@air-di-ludovico.station>
2021-06-08 15:29:17 +0200

    lala

--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,10 +1,27 @@
 package it.polito.tdp.imdb.model;
 
+import java.util.List;
+
 import it.polito.tdp.imdb.db.ImdbDAO;
 
 public class Model {
 
 	
 	private ImdbDAO dao;
+	
+	public Model() {
+		
+		
+		this.dao = new ImdbDAO();
+		
+	}
+	
+	
+	
+	
+	public List<String> getTuttiGeneri(){
+		return dao.listGeneri();
+		
+	}
 	
 }
